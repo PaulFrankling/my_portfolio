6570264c45fae2772eece63b1d3e18e9e8db39f8
PaulFrankling <franky.p@hotmail.co.uk>
2021-11-14 19:13:28 +0000

    added some js to make writing animated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
-document.getElementById('btn-one').addEventListener('click', function() {
+document.getElementById('btn-one').addEventListener('click', function () {
     var icon = this.querySelector('.arrow-one i.fas');
     icon.classList.toggle('rotate');
 });
 
-document.getElementById('btn-two').addEventListener('click', function() {
+document.getElementById('btn-two').addEventListener('click', function () {
     var icon = this.querySelector('.arrow-two i.fas');
     icon.classList.toggle('rotate');
 });
 
-document.getElementById('btn-three').addEventListener('click', function() {
+document.getElementById('btn-three').addEventListener('click', function () {
     var icon = this.querySelector('.arrow-three i.fas');
     icon.classList.toggle('rotate');
 });
 
-document.getElementById('btn-four').addEventListener('click', function() {
+document.getElementById('btn-four').addEventListener('click', function () {
     var icon = this.querySelector('.arrow-four i.fas');
     icon.classList.toggle('rotate');
+});
+
+let showSummary = function (target, message, index, interval) {
+    if (index < message.length) {
+        $(target).text($(target).text() + message[index++]);
+        setTimeout(function () { showSummary(target, message, index, interval); }, interval);
+    }
+}
+
+$(function () {
+    showSummary("#summary", "This portfolio was established whilst studying Full Stack Development at the Code Institute.", 0, 20);
 });
